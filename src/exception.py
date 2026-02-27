80ae29e2bc52bbfb2531f5752b0980a3e542fddc
MOHD RIYAN OZAIR <149253756+RiyanOzair@users.noreply.github.com>
2026-02-28 05:29:33 +0530

    created setup and requirements

---
+++ src/exception.py
@@ -0,0 +1,21 @@
+import sys
+
+def error_message_detail(error, error_detail):
+    _, _, exc_tb = error_detail.exc_info()
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+    else:
+        file_name = "Unknown"
+        line_number = "Unknown"
+    error_message = f"Error occurred in script: {file_name} at line number: {line_number} with error message: {str(error)}"
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
