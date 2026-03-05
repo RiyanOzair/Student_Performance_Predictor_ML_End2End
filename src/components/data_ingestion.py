04d15735bdd588d17e728032d16ee10e992dd568
MOHD RIYAN OZAIR <149253756+RiyanOzair@users.noreply.github.com>
2026-03-05 12:51:28 +0530

    data transformation completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,9 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+from src.utils import save_object
 
 @dataclass
 class DataIngestionConfig:
@@ -41,4 +44,7 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_path, test_path = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_path, test_path)
+    
